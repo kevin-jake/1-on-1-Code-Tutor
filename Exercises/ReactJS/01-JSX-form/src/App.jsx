7fc73e4b43083645703f5b795e11da2082def9d5
Kevin Jake <kevin_jake_04@yahoo.com>
2024-03-07 21:26:30 +0800

    add exercise

--- Exercises/ReactJS/01-JSX-form/src/App.jsx
+++ Exercises/ReactJS/01-JSX-form/src/App.jsx
@@ -13,20 +13,7 @@ Here's the raw HTML:
   </button>
 </form>
 */
-
   // PUT YOUR CODE HERE
-  return (
-    <form>
-      <label htmlFor="search-input">Search:</label>
-      <input id="search-input" />
-      <button aria-label="Submit" className="submit-btn">
-        <img
-          alt=""
-          src="https://sandpack-bundler.vercel.app/img/arrow-right.svg"
-        />
-      </button>
-    </form>
-  );
 }
 
 export default App;
