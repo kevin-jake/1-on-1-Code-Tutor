2fa994c74d132a909eb53a5c76a521f601597fd5
Kevin Jake <kevin_jake_04@yahoo.com>
2024-03-30 12:52:03 +0800

    add 07-Counterv2 exercise

--- Exercises/ReactJS/07-Counterv2/src/App.jsx
+++ Exercises/ReactJS/07-Counterv2/src/App.jsx
@@ -8,53 +8,9 @@ import {
   Hash,
 } from "react-feather";
 
+// FEEL FREE TO MODIFY THE COMPONENT BELOW
 function App() {
   const [count, setCount] = React.useState(0);
-
-  const increase = (isIncreaseBy10) => {
-    let inc = 0;
-    if (isIncreaseBy10) {
-      inc = count + 10;
-      setCount(inc);
-    } else {
-      inc = count + 1;
-      setCount(inc);
-    }
-  };
-
-  const reset = () => {
-    setCount(0);
-  };
-
-  const random = () => {
-    setCount(Math.round(Math.random() * 100));
-  };
-
-  const decrease = (isDecreaseBy10) => {
-    let dec = 0;
-    if (isDecreaseBy10) {
-      dec = count + 10;
-      setCount(dec);
-    } else {
-      dec = count + 1;
-      setCount(dec);
-    }
-  };
-
-  // ALTERNATE SOLUTION:
-  // const clamp = (val, min = 0, max = 1) => {
-  //   if (min > max) {
-  //     [min, max] = [max, min];
-  //   }
-
-  //   return Math.max(min, Math.min(max, val));
-  // };
-
-  // function Counter({ initialVal = 0 }) {
-  //   const [
-  //     count,
-  //     setCount,
-  //   ] = React.useState(initialVal);
 
   return (
     <div className="wrapper">
@@ -63,27 +19,27 @@ function App() {
         <span className="count">{count}</span>
       </p>
       <div className="button-row">
-        <button onClick={() => increase(false)}>
+        <button>
           <ChevronUp />
           <span className="visually-hidden">Increase slightly</span>
         </button>
-        <button onClick={() => increase(true)}>
+        <button>
           <ChevronsUp />
           <span className="visually-hidden">Increase a lot</span>
         </button>
-        <button onClick={reset}>
+        <button>
           <RotateCcw />
           <span className="visually-hidden">Reset</span>
         </button>
-        <button onClick={random}>
+        <button>
           <Hash />
           <span className="visually-hidden">Set to random value</span>
         </button>
-        <button onClick={() => decrease(true)}>
+        <button>
           <ChevronsDown />
           <span className="visually-hidden">Decrease a lot</span>
         </button>
-        <button onClick={() => decrease(false)}>
+        <button>
           <ChevronDown />
           <span className="visually-hidden">Decrease slightly</span>
         </button>
