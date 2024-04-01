becd383712945587f92c70b850929dda26745182
Kevin Jake <kevin_jake_04@yahoo.com>
2024-04-01 16:32:17 +0800

    Add solution on 10-2FA exercise

--- Exercises/ReactJS/10-2FA/src/App.jsx
+++ Exercises/ReactJS/10-2FA/src/App.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
+
 const CORRECT_CODE = "123456";
 
 // CHANGE THE CODE BELOW TO MEET THE ACCEPTANCE CRITERIA
 function App() {
+  const [code, setCode] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const isCorrect = code === CORRECT_CODE;
+    window.alert(isCorrect ? "Correct!" : "Incorrect");
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="auth-code">Enter authorization code:</label>
       <div className="row">
-        <input id="auth-code" type="text" required={true} maxLength={6} />
+        <input
+          id="auth-code"
+          type="text"
+          required={true}
+          maxLength={6}
+          value={code}
+          onChange={(event) => {
+            setCode(event.target.value);
+          }}
+        />
         <button>Validate</button>
       </div>
-    </>
+    </form>
   );
 }
 
